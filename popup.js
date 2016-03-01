9e3afe7bc3d0dfb474f822236a5010af46052032
T-tssxuan <2270360114@qq.com>
2016-03-02 00:44:59 +0800

    Finish the flow inspect function [1] Add the threshold for total flow. [2] Add the threshold for hour flow.

--- popup.js
+++ popup.js
@@ -36,14 +36,11 @@ function getLocalIPs(callback) {
     }, function onerror() {});
 }
 
-function getCookieAndCheckcode() {
-    var info = ["", ""];
-	// remove the request cookie
+function getCookieAndCheckcode(info) {
+	// remove the request cookie, for get the fresh cookie
 	chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
 		var headers = details.requestHeaders;
 		var blockingResponse = {};
-        console.log("in onBeforeSendHeaders: ");
-        console.log(headers);
 		for( var i = 0, l = headers.length; i < l; ++i ) {
 			if( headers[i].name == 'Cookie' ) {
 				headers.splice(i, 1);
@@ -58,14 +55,11 @@ function getCookieAndCheckcode() {
 
     // get the session cookie
 	chrome.webRequest.onHeadersReceived.addListener(function( details ) {
-		console.log(details);
 		details.responseHeaders.forEach(function(ele) {
 			if (ele.name == "Set-Cookie") {
 				var JSESSIONID = ele.value.substring(0, ele.value.indexOf(";"));
-                console.log("abc");
                 info[0] = JSESSIONID;
                 Cookie = JSESSIONID;
-				console.log("my Cookie: " + JSESSIONID);
 			}
 		});
 		return {responseHeaders:details.responseHeaders};
@@ -76,8 +70,6 @@ function getCookieAndCheckcode() {
 	chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
 		var headers = details.requestHeaders;
 		var blockingResponse = {};
-        console.log("in onBeforeSendHeaders: ");
-        console.log(headers);
 		for( var i = 0, l = headers.length; i < l; ++i ) {
 			if( headers[i].name == 'Cookie' ) {
                 console.log("#########################");
@@ -119,7 +111,7 @@ function getCookieAndCheckcode() {
 }
 
 
-function login(info) {
+function login(info, user_info) {
     chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
         var headers = details.requestHeaders;
         var blockingResponse = {};
@@ -160,16 +152,26 @@ function login(info) {
     xhr.onreadystatechange = function() {
         if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
             console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^");
+            console.log(xhr.responseText);
+            var str = xhr.responseText.replace(/(?:\r\n|\r|\n|\t)/g, "");
+            if (str.search("登 录") == -1) {
+                info[2] = true;
+            } else {
+                var html = '<div class="item"> Login Error </div>';
+                document.getElementById("terminals").innerHTML = html;
+            }
             console.log(xhr.getAllResponseHeaders());
             console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^");
         }
     }
-    pwd = calcMD5("44266");
-    data = "account=2015140391&password=" + pwd + "&code=&checkcode=" + info[1] + "&Submit=%E7%99%BB+%E5%BD%95";
+    var pwd = calcMD5(user_info["password"]);
+    data = "account=" + user_info["id"];
+    data += "&password=" + pwd + "&code=&checkcode=" + info[1] + "&Submit=%E7%99%BB+%E5%BD%95";
+    console.log("--------------->: " + data);
     xhr.send(data);
 }
 
-function tooffline(tag){//在线详单--强制离线
+function tooffline(tag){
     var url = "http://gwself.bupt.edu.cn/tooffline?t=" + Math.random();
     url += "&fldsessionid=" + tag;
     console.log("!!!!!!!!!!!!!!!!!!!!!!!" + url + "!!!!!!!!!!!!!!!!!");
@@ -201,13 +203,22 @@ function tooffline(tag){//在线详单--强制离线
     xhr.send();
 }
 
-function getTerminalInfo() {
-    var info = getCookieAndCheckcode();
-    console.log(info);
+var log_info = ["", "", false, 0];
+function getTerminalInfo(user_info) {
+    console.log(log_info);
+    if (!log_info[2]) {
+        getCookieAndCheckcode(log_info);
+    }
     setTimeout(function() {
-        login(info);
-        console.log("after login");
+        if (!log_info[2] && log_info[3] < 3) {
+            login(log_info, user_info);
+            log_info[3] ++;
+            console.log("after login");
+        }
         setTimeout(function() {
+            if (!log_info[2]) {
+                return;
+            }
             var xhr = new XMLHttpRequest();
             console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%");
             xhr.open("GET", "http://gwself.bupt.edu.cn/nav_offLine");
@@ -221,6 +232,10 @@ function getTerminalInfo() {
                         var tags = str[0].match(/<td style="display:none;">\d+?<\/td>/g);
                         var term = document.getElementById("terminals");
                         term.innerHTML = "";
+                        if (ips == null) {
+                            term.innerHTML = '<div class="item">No terminal.</div>';
+                            return;
+                        }
                         for (var i = 0; i < tags.length; i++ ) {
                             tags[i] = /\d+/g.exec(tags[i])[0];
                         }
@@ -253,7 +268,6 @@ function getTerminalInfo() {
                                 console.log(this.lastChild);
                             });
 
-
                             div.appendChild(ip);
                             div.appendChild(logoff);
                             term.appendChild(div);
@@ -269,38 +283,96 @@ function getTerminalInfo() {
 
 function getFlowInfo(url) {
     var xhr = new XMLHttpRequest();
+    var url = document.getElementById("gateway").value;
     xhr.open("GET", url);
     xhr.onreadystatechange = function() {
         if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
             var content = xhr.responseText.replace(/(?:\r\n|\r|\n|\t|\s)/g, "");
-            var tmp = /time=.*?;/g.exec(content);
-            var time = Number(/\d+/g.exec(tmp[0])[0]);
-            var tmp = /flow=.*?;/g.exec(content);
-            var flow = Number(/\d+/g.exec(tmp[0])[0]) / 1024;
-            var tmp = /fee=.*?;/g.exec(content);
-            var fee = Number(/\d+/g.exec(tmp[0])[0]) / 10000;
-
-            console.log("time: " + time);
-            console.log("flow: " + flow);
-            console.log("fee: " + fee);
-
             var ele = document.getElementById("flow");
-            ele.innerHTML = "";
-            var html = "";
-            html += '<div class="item"> Used time: ' + time + ' Min </div>';
-            if (flow > 1024) {
-                flow = flow / 1024;
-                flow = flow.toFixed(3);
-                html += '<div class="item"> Used internet traffic: ' + flow + ' Gbyte </div>';
+            if (content.search("欢迎登录北邮校园网络") != -1) {
+                var html = '<div class="item"> Please login gateway! </div>';
+                ele.innerHTML = html;
             } else {
-                flow = flow.toFixed(3);
-                html += '<div class="item"> Used internet traffic: ' + flow + ' Mbyte </div>';
+                var tmp = /time=.*?;/g.exec(content);
+                var time = Number(/\d+/g.exec(tmp[0])[0]);
+                var tmp = /flow=.*?;/g.exec(content);
+                var flow = Number(/\d+/g.exec(tmp[0])[0]) / 1024;
+                var tmp = /fee=.*?;/g.exec(content);
+                var fee = Number(/\d+/g.exec(tmp[0])[0]) / 10000;
+
+                console.log("time: " + time);
+                console.log("flow: " + flow);
+                console.log("fee: " + fee);
+
+                ele.innerHTML = "";
+                var html = "";
+                html += '<div class="item"> Used time: ' + time + ' Min </div>';
+                if (flow > 1024) {
+                    flow = flow / 1024;
+                    flow = flow.toFixed(3);
+                    html += '<div class="item"> Used internet traffic: ' + flow + ' Gbyte </div>';
+                } else {
+                    flow = flow.toFixed(3);
+                    html += '<div class="item"> Used internet traffic: ' + flow + ' Mbyte </div>';
+                }
+                html += '<div class="item"> Balance: ' + fee + ' RMB </div>';
+                ele.innerHTML = html;
             }
-            html += '<div class="item"> Balance: ' + fee + ' RMB </div>';
-            ele.innerHTML = html;
         }
     }
     xhr.send();
+}
+
+function saveUserInfo(user_info) {
+    chrome.storage.sync.set(user_info, function() {
+        console.log("--->saved user_info");
+    });
+}
+
+function getUserInfo(user_info) {
+    var arr = ["id", "password", "gateway", "hour_max", "hour_max_switch", "threshold", "threshold_switch"];
+    chrome.storage.sync.get(arr, function(info) {
+        if ("id" in info) {
+            document.getElementById("id").value = info["id"];
+            user_info["id"] = info["id"];
+        }
+        if ("password" in info) {
+            document.getElementById("password").value = info["password"];
+            user_info["password"] = info["password"];
+        }
+        if ("gateway" in info) {
+            document.getElementById("gateway").value = info["gateway"];
+            user_info["gateway"] = info["gateway"];
+        }
+        if ("hour_max" in info) {
+            document.getElementById("hour_max").value = info["hour_max"];
+            user_info["hour_max"] = info["hour_max"];
+        }
+        if ("hour_max_switch" in info) {
+            document.getElementById("hour_max_switch").checked = info["hour_max_switch"];
+            user_info["hour_max_switch"] = info["hour_max_switch"];
+        }
+        if ("threshold" in info) {
+            document.getElementById("threshold").value = info["threshold"];
+            user_info["threshold"] = info["threshold"];
+        }
+        if ("threshold_switch" in info) {
+            document.getElementById("threshold_switch").checked= info["threshold_switch"];
+            user_info["threshold_switch"] = info["threshold_switch"];
+        }
+    });
+}
+
+function updateBasicInfo(user_info) {
+	// get flow information
+    getFlowInfo();
+    if (("id" in user_info) && ("password" in user_info)) {
+        getTerminalInfo(user_info);
+    }
+    console.log(user_info);
+    setTimeout(function() {
+        updateBasicInfo(user_info);
+    }, 5000);
 }
 
 window.onload = function() {
@@ -315,13 +387,43 @@ window.onload = function() {
         });
     });
 
-	// get flow information
-    getFlowInfo("http://10.3.8.211/");
-    setInterval(function() {
-        getFlowInfo("http://10.3.8.211/");
-    }, 5000);
+    var user_info = {};
+    getUserInfo(user_info);
+    updateBasicInfo(user_info);
 
-    // get terminal information
-    getTerminalInfo();
+    document.getElementById("save").addEventListener("click", function() {
+        user_info["id"] = document.getElementById("id").value;
+        user_info["password"] = document.getElementById("password").value;
+        user_info["gateway"] = document.getElementById("gateway").value;
+        user_info["hour_max"] = document.getElementById("hour_max").value;
+        user_info["hour_max_switch"] = document.getElementById("hour_max_switch").checked;
+        user_info["threshold"] = document.getElementById("threshold").value;
+        user_info["threshold_switch"] = document.getElementById("threshold_switch").checked;
+        log_info[3] = 0;
+        saveUserInfo(user_info);
+    });
 
+    document.getElementById("login_gateway").addEventListener("click", function() {
+        var xhr = new XMLHttpRequest();
+        var id = document.getElementById("id").value;
+        var password = document.getElementById("password").value;
+        var gateway = document.getElementById("gateway").value;
+        xhr.open("POST", gateway);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        var data = "DDDDD=" + id + "&upass=" + password + "&0MKKey=";
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+                var content = xhr.responseText.replace(/(?:\r\n|\r|\n|\t|\s)/g, "");
+                var ele = document.getElementById("flow");
+                var html = "";
+                if (content.search("账号或密码不对") != -1) {
+                    html = '<div class="item"> Login Gateway Error! </div>';
+                } else {
+                    html = 'Login Gateway Successfully!';
+                }
+                ele.innerHTML = html;
+            }
+        }
+        xhr.send(data);
+    });
 }
